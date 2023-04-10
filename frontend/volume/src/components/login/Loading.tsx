2b2da1d446a2d2c3536a3dbb190038f2e7662fd1
Justin Hille <jhille@f1r2s11.codam.nl>
2023-04-10 17:36:13 +0200

    temporary loading screen

--- frontend/volume/src/components/login/Loading.tsx
+++ frontend/volume/src/components/login/Loading.tsx
@@ -1,9 +1,17 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import "src/styles/style.css";
 
-function Loading({ LogIn }: { LogIn(): void }) {
+function Loading() {
+	const navigate = useNavigate();
+	const toHomePage = () => {
+		useEffect(() => {
+			navigate("/");
+		});
+	};
 	return (
 		<div id="loading">
-			<div onClick={LogIn} className="login">
+			<div onClick={toHomePage} className="login">
 				loading...
 				<img className="logo42" src="/img/42logo.svg" />
 			</div>
@@ -11,3 +19,15 @@ function Loading({ LogIn }: { LogIn(): void }) {
 	);
 }
 export default Loading;
+
+// function Loading({ LogIn }: { LogIn(): void }) {
+// 	return (
+// 		<div id="loading">
+// 			<div onClick={LogIn} className="login">
+// 				loading...
+// 				<img className="logo42" src="/img/42logo.svg" />
+// 			</div>
+// 		</div>
+// 	);
+// }
+// export default Loading;
