77bdf777ea1f752ada4146775b4c60c8eeeeed85
Mariusmivw <mvan-wij@student.codam.nl>
2023-04-12 16:51:42 +0200

    make `CreateChatInput` an ArgsType instead of InputType

--- backend/volume/src/chat/chat.resolver.ts
+++ backend/volume/src/chat/chat.resolver.ts
@@ -1,23 +1,26 @@
-import { Args, Info, Mutation, Parent, Query, ResolveField, Resolver, Subscription } from '@nestjs/graphql';
+import {
+	Args,
+	Mutation,
+	Parent,
+	Query,
+	ResolveField,
+	Resolver,
+} from '@nestjs/graphql';
 import { Chat } from './entities/chat.entity';
 import { CreateChatInput } from './dto/create-chat.input';
 import { ChatService } from './chat.service';
-import { inspect } from 'util';
 
 @Resolver((of) => Chat)
 export class ChatResolver {
-	constructor(
-		private readonly chat_service: ChatService,
-	) {}
+	constructor(private readonly chat_service: ChatService) {}
 
 	@Query((returns) => [Chat])
 	async all_chats() {
-		// console.log(inspect(info["fieldNodes"][0]["selectionSet"], { depth: 4, colors: true }));
 		return this.chat_service.getAllChats();
 	}
 
 	@Mutation((returns) => Chat, { nullable: true })
-	async create_chat(@Args('chat') chat: CreateChatInput) {
+	async createChat(@Args() chat: CreateChatInput) {
 		return this.chat_service.create(chat);
 	}
 
--- backend/volume/src/chat/dto/create-chat.input.ts
+++ backend/volume/src/chat/dto/create-chat.input.ts
@@ -1,7 +1,9 @@
-import { InputType, Field } from '@nestjs/graphql';
+import { Field, ArgsType } from '@nestjs/graphql';
 
-@InputType()
+@ArgsType()
 export class CreateChatInput {
-	@Field(() => [String], { description: 'id\'s of all the members in this chat' })
+	@Field(() => [String], {
+		description: "id's of all the members in this chat",
+	})
 	member_ids: string[];
 }
