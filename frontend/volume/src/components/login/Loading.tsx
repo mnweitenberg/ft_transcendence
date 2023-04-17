0e7fa801945b68e7c38f787a752c4c7072e471a3
Justin Hille <jhille@f1r5s14.codam.nl>
2023-04-17 14:16:46 +0200

    working link to homepage

--- frontend/volume/src/components/login/Loading.tsx
+++ frontend/volume/src/components/login/Loading.tsx
@@ -1,12 +1,15 @@
+import { Link, useNavigate } from "react-router-dom";
 import "src/styles/style.css";
 
 function Loading({ LogIn }: { LogIn(): void }) {
 	return (
 		<div id="auth">
-			<div onClick={LogIn} className="signin">
-				loading...
-				<img className="logo42" src="/img/42logo.svg" />
-			</div>
+			<Link to="/">
+				<div onClick={LogIn} className="signin">
+					loading...
+					<img className="logo42" src="/img/42logo.svg" />
+				</div>
+			</Link>
 		</div>
 	);
 }
