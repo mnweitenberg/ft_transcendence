9655882e9a0efd501eaacc8a335d7f4b8854f294
Justin Hille <jhille@f1r2s10.codam.nl>
2023-05-24 14:10:58 +0200

    removed unnecessary dotenv imports

--- backend/volume/src/auth/strategies/jwt.strategy.ts
+++ backend/volume/src/auth/strategies/jwt.strategy.ts
@@ -2,9 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import * as dotenv from 'dotenv';
 import { UserInfo } from '../auth.service';
-dotenv.config();
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
