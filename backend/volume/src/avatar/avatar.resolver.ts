71ee565169a17f6d1b66bf66593104e78fc5e587
Justin Hille <jhille@f1r3s6.codam.nl>
2023-05-29 16:48:17 +0200

    not working

--- backend/volume/src/avatar/avatar.resolver.ts
+++ backend/volume/src/avatar/avatar.resolver.ts
@@ -1,1 +1,22 @@
+import { Args, Mutation, Resolver } from "@nestjs/graphql";
+import { AvatarService } from "./avatar.service";
+import { GraphQLUpload } from 'graphql-upload/GraphQLUpload.mjs';
 
+@Resolver((of) => Avatar)
+export class AvatarResolver {
+  constructor(private avatarService: AvatarService) {}
+
+  @Mutation((returns) => Avatar)
+  async createAvatar(@Args({ name: 'file', type: () => GraphQLUpload }) file: FileUpload)
+  {
+    let readStream = file.createReadStream()
+    let data = ''
+    readStream.once('error', err => {
+      return console.log(err)
+    })
+    readStream.on('data', chunk => (data += chunk))
+    readStream.on('end', () => {
+  this.userService.createUser(email, age, Buffer.from(data, 'binary'))
+})
+  }
+}
--- backend/volume/src/main.ts
+++ backend/volume/src/main.ts
@@ -2,11 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import * as dotenv from 'dotenv';
-<<<<<<< HEAD
-dotenv.config()
-=======
 dotenv.config();
->>>>>>> master
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
