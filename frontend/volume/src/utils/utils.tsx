6784a6c11df1b289b03432ff4b44d78a80f3c6e4
Milan Weitenberg <mweitenb@f1r4s13.codam.nl>
2023-06-12 17:55:58 +0200

    small fix

--- frontend/volume/src/utils/utils.tsx
+++ frontend/volume/src/utils/utils.tsx
@@ -12,7 +12,7 @@ export function createChallengeAlert(user: any, propsModal: i.ModalProps) {
 export function createFriendRequesAlert(user: any, propsModal: i.ModalProps) {
 	return (
 		<div className="alert">
-			<h3>Send friend request to {user}? </h3>
+			<h3>Send friend request to {user.username}? </h3>
 			<button onClick={() => propsModal.setShowModal(false)}>Yes</button>
 		</div>
 	);
@@ -21,7 +21,7 @@ export function createFriendRequesAlert(user: any, propsModal: i.ModalProps) {
 export function createBlockAlert(user: any, propsModal: i.ModalProps) {
 	return (
 		<div className="alert">
-			<h3>Are you sure you want to block {user}? </h3>
+			<h3>Are you sure you want to block {user.username}? </h3>
 			<button onClick={() => propsModal.setShowModal(false)}>Yes</button>
 		</div>
 	);
