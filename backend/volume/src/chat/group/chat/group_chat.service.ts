876c7b17db52c7204f0226efcc686504516bb5a9
milan <m.n.weitenberg@gmail.com>
2023-06-28 22:25:20 +0200

    replaces userId by JwtAAuthGaurd

--- backend/volume/src/chat/group/chat/group_chat.service.ts
+++ backend/volume/src/chat/group/chat/group_chat.service.ts
@@ -6,6 +6,10 @@ import { UserService } from 'src/user/user.service';
 import { CreateGroupChannelInput } from './dto/create_group_chat.input';
 import { User } from 'src/user/entities/user.entity';
 import { GroupMessage } from '../message/entities/group_message.entity';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { AuthUser } from 'src/auth/decorators/auth-user.decorator';
+import { UserInfo } from 'src/auth/auth.service';
 
 @Injectable()
 export class GroupChatService {
@@ -37,16 +41,16 @@ export class GroupChatService {
 		return await this.channelRepository.save(channel);
 	}
 
-	async join(userId: string, channelId: string): Promise<GroupChat> {
+	@UseGuards(JwtAuthGuard)
+	async join(@AuthUser() userInfo: UserInfo, channelId: string): Promise<GroupChat> {
 		const channel = await this.getChannelById(channelId);
-		const user = await this.userService.getUserById(userId);
+		const user = await this.userService.getUserById(userInfo.userUid);
 	
 		if (!channel) throw new Error(`Channel with id ${channelId} does not exist`);
 		channel.members = await this.getMembers(channel);
 		channel.members.push(user);
 		return await this.channelRepository.save(channel);
 	  }
-	
 	
 	async getMembers(channel: GroupChat): Promise<Array<User>> {
 		const channel_with_members = await this.channelRepository.findOne({
