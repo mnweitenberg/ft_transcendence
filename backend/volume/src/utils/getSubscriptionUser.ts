183fe423a350243fd31ed8e152f5895461bc74e1
Jonathan Bedaux <83814959+mo-bedo@users.noreply.github.com>
2023-07-04 14:08:17 +0200

    Setup for auth on subscription

---
+++ backend/volume/src/utils/getSubscriptionUser.ts
@@ -0,0 +1,6 @@
+import { UserInfo } from "src/auth/user-info.interface";
+import { GraphQLContext } from "./graphql-context";
+
+export function getSubscriptionUser({req: {extra: {request: {user}}}}: GraphQLContext): UserInfo {
+	return user;
+}
